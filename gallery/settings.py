0419e73cec877d464f43185c514ab692da3b4f53
Mike Munene <you@example.com>
2020-11-14 15:24:14 +0300

    image, category and location tables created

--- gallery/settings.py
+++ gallery/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER': 'postgres',
+        'PASSWORD': 'itwaruchiu',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
--- pics/models.py
+++ pics/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+    Image_location = models.CharField(max_length=30)
+
+class Category(models.Model):
+    Image_category = models.CharField(max_length=30)
+
+class Image(models.Model):    
+    Image_name = models.CharField(max_length=30)
+    Image_description = models.CharField(max_length=30)
+    location = models.ForeignKey(Location, null=True, on_delete=models.SET_NULL)
+    Category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.Image_name
+
+
+
+
