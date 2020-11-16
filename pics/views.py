56243161a4596426f403ff478a100f19e57f59ac
Mike Munene <you@example.com>
2020-11-16 06:11:43 +0300

    create search view

--- pics/views.py
+++ pics/views.py
@@ -14,3 +14,15 @@ def location(request,location):
     selected_location = Location.objects.get(id = location)
     images = Image.objects.filter(location = selected_location.id)
     return render(request, 'location.html', {"location":selected_location,"locations":locations,"images":images})
+
+def search(request):
+    if 'imagesearch' in request.GET and request.GET["imagesearch"]:
+        search_term = request.GET.get("category")
+        category = request.GET.get("imagesearch")
+        searched_images = Image.search_by_category(category)
+        message = f"{category}"
+        print(searched_images)
+        return render(request,'search.html',{"images":searched_images,"category":search_term})
+    else:
+        message = "Try entering a valid category"
+        return render(request, 'search.html', {"message": message})
