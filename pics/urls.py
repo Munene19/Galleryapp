2325e02eeedc2c269f4fdc18bb85d2bc282f2c99
Mike Munene <you@example.com>
2020-11-16 06:14:43 +0300

    tests created

--- pics/urls.py
+++ pics/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('home/',views.index, name = 'home')
+    path('home/',views.index, name = 'home'),
+    path('location/',views.location,name = 'location'),
+    path('search/',views.search,name='search')
 ]
 
