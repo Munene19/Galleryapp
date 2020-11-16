fcaedc205e28322e3da36b699ef154690630f6cf
Mike Munene <you@example.com>
2020-11-16 06:10:07 +0300

    update location view

--- pics/views.py
+++ pics/views.py
@@ -8,3 +8,9 @@ def index(request):
     images = Image.all_images()
     locations = Location.objects.all()
     return render(request, 'index.html', {"images":images,"locations":locations})
+
+def location(request,location):
+    locations = Location.objects.all()
+    selected_location = Location.objects.get(id = location)
+    images = Image.objects.filter(location = selected_location.id)
+    return render(request, 'location.html', {"location":selected_location,"locations":locations,"images":images})
